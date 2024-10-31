74a960bcd0f90e1410a28c33c487ec56327a7d6e
devyaden <dev@yadnxd.com>
2024-10-31 10:41:04 +0300

    soso

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    domains: ['img.icons8.com']
+}
 };
 
 export default nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import localFont from "next/font/local";
-
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -11,105 +10,55 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+import Link from 'next/link'
+const socialMedia = [
+  { name: 'Facebook', url: 'https://www.facebook.com', logo: 'https://img.icons8.com/?size=256&id=118497&format=png' },
+  { name: 'x', url: 'https://x.com/yadnxd?t=xESgy0TMXyGCJivzdbRQ0A&s=09', logo: 'https://img.icons8.com/?size=256&id=ClbD5JTFM7FA&format=png' },
+  { name: 'Instagram', url: 'https://www.instagram.com/yadnxd?igsh=Y2xpYzFncWVwdzY5',
+     logo: 'https://img.icons8.com/?size=256&id=32323&format=png' },
+  { name: 'LinkedIn', url: 'https://www.linkedin.com/company/yadnxd/', 
+    logo: 'https://img.icons8.com/?size=256&id=13930&format=png' },
+  { name: 'TikTok', url: 'http://www.tiktok.com/@yadnxd', logo: 'https://img.icons8.com/?size=256&id=118640&format=png' },
+  { name: 'TikTok', url: 'mailto:info@yadnxd.com', 
+    logo: 'https://img.icons8.com/?size=100&id=19408&format=png&color=000000' },
+    { name: 'TikTok', url: 'tel:966550513539', 
+      logo: 'https://img.icons8.com/?size=100&id=44208&format=png&color=2d3194'
+ },
+ { name: 'TikTok', url: 'https://maps.app.goo.gl/ZMZZ8cKnNqEyAyJ66', 
+  logo: 'https://img.icons8.com/?size=100&id=107436&format=png&color=000000'
+},
+];
 
+import Original_logo_colors from "../public/Original_logo_colors.png"
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <div
+      className={`${geistSans.variable} ${geistMono.variable} grid bg-gray-900 grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      >
+        <Link href={'https://www.yadnxd.com'} className="m-20" >
+      <Image className="  m-10"
+        height={200}
+        width={200} 
+        src={Original_logo_colors} 
+        alt={""}      
+      ></Image></Link>
+   
+       
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+       
+       <div className=" grid  grid-cols-2 gap-4">
+            { socialMedia.map(( d: any, i: any ) => (
+              <Link href={d.url}key={i}>
+              <div className=" card  bg-green-700"   >
+              <Image
+        height={200}
+        width={200} 
+        src={d.logo} 
+        alt={""}      
+      ></Image>
+              </div></Link>
+ ))}
+       </div>
     </div>
   );
 }
